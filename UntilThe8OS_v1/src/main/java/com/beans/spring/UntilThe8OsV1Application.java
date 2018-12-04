bef4df440ad5f3e222621396f06f48b54d42b6dc
rebeaceitunobueno <45035950+rebeaceitunobueno@users.noreply.github.com>
2018-12-04 16:18:16 +0100

    PRUEBA

---
+++ UntilThe8OS_v1/src/main/java/com/beans/spring/UntilThe8OsV1Application.java
@@ -0,0 +1,12 @@
+package com.beans.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntilThe8OsV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntilThe8OsV1Application.class, args);
+	}
+}
