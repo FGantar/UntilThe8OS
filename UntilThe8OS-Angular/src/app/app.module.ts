c658f8cc9da6c308045b0fc32530741707df4da9
rebeaceitunobueno <45035950+rebeaceitunobueno@users.noreply.github.com>
2018-12-12 11:22:01 +0100

    ejercicio
    holahghgtrh

--- UntilThe8OS-Angular/src/app/app.module.ts
+++ UntilThe8OS-Angular/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonaEditComponent } from './persona-edit/persona-edit.component';
+import { HolaComponent } from './hola/hola.component';
+import { AdiosComponent } from './adios/adios.component';
+import { PersonaListComponent } from './persona-list/persona-list.component';
+import { SharedComponent } from './shared/shared.component';
+import { PersonaComponent } from './shared/persona/persona.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaEditComponent,
+    HolaComponent,
+    AdiosComponent,
+    PersonaListComponent,
+    SharedComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule,
