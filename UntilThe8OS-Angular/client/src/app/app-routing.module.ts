ba45bca8ca724420f94d0b11a49e0c9fe58907d0
Cristian <cristonygf@gmail.com>
2018-12-12 19:49:27 +0100

    app routing

--- UntilThe8OS-Angular/client/src/app/app-routing.module.ts
+++ UntilThe8OS-Angular/client/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PersonaAddComponent } from './persona-add/persona-add.component';
+import { PersonaListComponent } from './persona-list/persona-list.component';
+import { PersonaEditService } from './shared/persona-edit/persona-edit.service';
+
+const routes: Routes = [
+  { path: 'listaContactos', component: PersonaListComponent },
+  { path: 'altaContacto', component: PersonaAddComponent },
+  { path: 'modificarContacto', component: PersonaEditService }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
 })
 export class AppRoutingModule { }
