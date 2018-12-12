4dc7de296e007b3457ac98a1b9b372d5256c23ba
Cristian <cristonygf@gmail.com>
2018-12-12 16:10:55 +0100

    altacontacto componente

--- UntilThe8OS-Angular/client/src/app/persona-add/persona-add.component.ts
+++ UntilThe8OS-Angular/client/src/app/persona-add/persona-add.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { PersonaAddService } from '../shared/persona-add/persona-add.service';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-persona-add',
   templateUrl: './persona-add.component.html',
-  styleUrls: ['./persona-add.component.css']
+  styleUrls: []
 })
 export class PersonaAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,private router: Router, private addService: PersonaAddService) { }
+
+  addForm: FormGroup;
 
   ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      id: [],
+      nombre: ['', Validators.required],
+      apellido1: ['', Validators.required],
+      apellido2: ['', Validators.required],
+      dni: ['', Validators.required],
+      fechaNacimiento: ['', Validators.required],
+      telefonos: ['', Validators.required],
+      direcciones: ['', Validators.required]
+    });
+
   }
 
+  onSubmit() {
+    this.addService.altaContacto(this.addForm.value)
+      .subscribe( data => {
+        this.router.navigate(['persona-list']);
+      });
+  }
 }
