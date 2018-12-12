f1ea5f91e7c31cdca48bdf1359e418a1270fa1ff
Cristian <cristonygf@gmail.com>
2018-12-12 15:52:12 +0100

    añado ruta buena pa observable

--- UntilThe8OS-Angular/client/src/app/shared/persona/persona.service.ts
+++ UntilThe8OS-Angular/client/src/app/shared/persona/persona.service.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable()
 export class PersonaService {
