69f689e1f6afbf689ad03028630b19c9cda12348
rebeaceitunobueno <45035950+rebeaceitunobueno@users.noreply.github.com>
2018-12-13 12:38:28 +0100

    que bonito

--- UntilThe8OS_REST/src/main/java/com/beansspringREST/config/ApplicationContextConfig.java
+++ UntilThe8OS_REST/src/main/java/com/beansspringREST/config/ApplicationContextConfig.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
@@ -22,10 +23,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.regex.Pattern;
 
 @Configuration
-@EnableWebMvc
+@EnableSwagger2
 @ComponentScan("com.beans.spring")
 @EnableTransactionManagement
-public class ApplicationContextConfig{
+public class ApplicationContextConfig {
 
 	@Autowired
 	ApplicationContext applicationContext;
@@ -52,21 +53,28 @@ public class ApplicationContextConfig{
 		viewResolver.setTemplateEngine(templateEngine());
 		return viewResolver;
 	}
-	
 
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()                 .apis(RequestHandlerSelectors.basePackage("guru.springframework.controllers"))
-                .paths(PathSelectors.regex("/product.*"))
-                .build();
-    }
-	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
 
-    
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/docApi/v2/api-docs", "/v2/api-docs");
+		registry.addRedirectViewController("/docApi/swagger-resources/configuration/ui",
+				"/swagger-resources/configuration/ui");
+		registry.addRedirectViewController("/docApi/swagger-resources/configuration/security",
+				"/swagger-resources/configuration/security");
+		registry.addRedirectViewController("/docApi/swagger-resources", "/swagger-resources");
+	}
+
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-		
+		registry.addResourceHandler("/docApi/swagger-ui.html**")
+				.addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+		registry.addResourceHandler("/docApi/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+
 	}
 
 }
